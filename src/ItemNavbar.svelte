82c5479d2f50b4456fbe05486e02cb7a5a40a6c3
Bright Amoateng <bright.amoateng150@gmail.com>
2023-02-20 23:25:50 -0500

    Indicate which item view is active

--- src/ItemNavbar.svelte
+++ src/ItemNavbar.svelte
@@ -1,21 +1,35 @@
 <script>
     import { createEventDispatcher } from "svelte";
 
-    let dispatch = createEventDispatcher();
-
     const VIEW_EVENT = "viewchange";
+    const View = {
+        ARTWORKS: "artwork",
+        DESCRIPTIONS: "descriptions",
+        TAGS: "tags"
+    };
 
-    function onClick(type) {
+    let activeView = View.ARTWORKS;
+
+    const dispatch = createEventDispatcher();
+
+    function onClick(view) {
         return () => {
-            dispatch(VIEW_EVENT, type);
+            activeView = view;
+            dispatch(VIEW_EVENT, view);
         };
     }
 </script>
 
 <div class="navbar">
-    <button on:click={onClick("descriptions")}>View <span class="red">Descriptions</span></button>
-    <button on:click={onClick("artwork")}>View <span class="red">Artwork</span></button>
-    <button on:click={onClick("tags")}>View <span class="red">Tags</span></button>
+    <button on:click={onClick(View.DESCRIPTIONS)} class:active={activeView === View.DESCRIPTIONS}>
+        View <span class="red">Descriptions</span>
+    </button>
+    <button on:click={onClick(View.ARTWORKS)} class:active={activeView === View.ARTWORKS}>
+        View <span class="red">Artwork</span>
+    </button>
+    <button on:click={onClick(View.TAGS)} class:active={activeView === View.TAGS}>
+        View <span class="red">Tags</span>
+    </button>
     <button disabled>Shuffle <span class="red">Items</span></button>
 </div>
 
